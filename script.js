7f4118f3e391da562f07e26114fca26c731ccd34
Emmanuel Lucio Urbina <emmanuelluur@gmail.com>
2019-11-01 01:47:41 -0500

    Checkout task :smile:

--- script.js
+++ script.js
@@ -4,54 +4,12 @@ const classNames = {
   TODO_TEXT: 'todo-text',
   TODO_DELETE: 'todo-delete',
 }
-
 const TODO_LIST = []
-
+const TODO_LIST_CHECKED = []
 const list = document.getElementById('todo-list')
 const itemCountSpan = document.getElementById('item-count')
 const uncheckedCountSpan = document.getElementById('unchecked-count')
 const app = document.getElementById("form-app")
-
-
-function main_app() {
-  app.innerHTML = `<button class="button center" onClick="newTodo()">New TODO</button>` 
-}
-
-function newTodo() {
-  app.innerHTML = FORM_NEW_TODO
-}
-
-function addTodo() {
-  let item = document.getElementById("todo").value
-  if (item.length < 5) {
-    alert("Task must be greater than five characters")
-  } else {
-    TODO_LIST.push(item)
-  }
-  list.innerHTML = loadTodo()
-  main_app()
-  itemCountSpan.innerHTML = total(TODO_LIST)
-}
-
-function cancel(){
-  main_app()
-}
-
-function loadTodo(){
-  let _arr = ""
-  TODO_LIST.forEach(el=>{
-    _arr+=`<li>${el}</li>`
-  })
-  return _arr
-}
-
-function total(item){
-  if(!item) {
-    return 0
-  }
-  return item.length
-}
-
 const FORM_NEW_TODO = `
 <p>
 <label for='todo'>Task:</label><br>
@@ -62,6 +20,76 @@ const FORM_NEW_TODO = `
 <button class="button center" onClick="cancel()">Cancel</button>
 </p>
 `;
+
+
+function main_app() {
+  /** main button */
+  app.innerHTML = `<button class="button center" onClick="newTodo()">New TODO</button>` 
+}
+
+function newTodo() {
+  /** load form new todo */
+  app.innerHTML = FORM_NEW_TODO
+}
+
+function addTodo() {
+  /** get task & valid non empty value */
+  let item = document.getElementById("todo").value
+  if (item.length < 5) {
+    alert("Task must be greater than five characters")
+  } else {
+    TODO_LIST.push(item)
+  }
+  // load todo list & return button new todo
+  list.innerHTML = loadTodo()
+  main_app()
+  changeTotalValues()
+}
+
+function cancel(){
+  /** load main button */
+  main_app()
+}
+
+function loadTodo(){
+  let _arr = ""
+  TODO_LIST.forEach(el=>{
+    _arr+=`<li><input type = 'checkbox' onClick = "checkoutTodo('${el}')"  id = '${el}'>   ${el}</li>`
+  })
+  return _arr
+}
+
+function total(item){
+  /** if not arg or is not array return 0 */
+  if(!item || typeof(item) != 'object' ) {
+    return 0
+  }
+  return item.length
+}
+
+function uncheckedTodo(item){
+  TODO_LIST.splice(item,1)
+  TODO_LIST_CHECKED.push(item)
+  list.innerHTML = loadTodo()
+  return TODO_LIST_CHECKED.length
+}
+
+function checkoutTodo(item){
+  uncheckedTodo(item)
+  let _arr = ""
+  TODO_LIST_CHECKED.forEach(el=>{
+    _arr+=`<li>${el} </li>`
+  })
+  changeTotalValues()
+  document.getElementById('todo-list-done').innerHTML=_arr
+}
+
+function changeTotalValues(){
+  itemCountSpan.innerHTML = total(TODO_LIST) + total(TODO_LIST_CHECKED)
+  uncheckedCountSpan.innerHTML = total(TODO_LIST)
+}
+
+
 
 document.addEventListener("DOMContentLoaded", () =>{
   main_app();
